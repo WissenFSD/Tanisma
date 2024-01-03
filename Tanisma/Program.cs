b7efae188fbe2fc104de41207fee4b949b0e57cc
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-03 21:58:31 +0300

    Add project files.

---
+++ Tanisma/Program.cs
@@ -0,0 +1,160 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+
+//Ekrana yazı yazdırma kodu
+
+
+
+/*Çoklu satır yorum satırı
+ 
+ */
+//Console.WriteLine("Merhaba Dünya");
+
+// Yazılı içerisinde metinsel ifadeler kesinlikle "" içerisinde olmalıdır.
+
+
+// aşağıdaki yanlış bir örnektir
+//Console.WriteLine(Merhaba Dünya);{}
+
+
+
+
+// PlaceHolder
+
+
+//Placa Holder içerisinde, virgülden sonra sırası 0 dan başlayarak yazılan değerler gelecektir.
+//Console.WriteLine("Toplam : {0} {4}", 10 , 20,30,50,100);
+
+// 0'ındı veri 10 değeridir
+// 4. veri 100 değeridir
+
+
+
+
+Console.WriteLine("İsmizi Giriniz : ");
+
+
+
+// Ekrandan değer alabilirim
+//Console.ReadLine();
+
+
+// Readline ekrandan değerler alabilirsiniz.!!
+
+//Console.WriteLine("Merhaba {0}",Console.ReadLine());
+
+
+//Console.WriteLine("Kaç yaşındasın");
+
+
+//Console.WriteLine("Girdiğin değer : {0}", Console.ReadLine());
+
+
+
+
+
+
+
+// ilk değişken tanımlama
+
+
+// metin tabanlı bir değişken tanımlamak için "" kullanmak zorundasınız.!!
+string ilkdegisken = "Ben metinsel bir değişkenim";
+
+
+// int tipinde değişken tanımlama
+int sayisaldegisken = 10;
+
+
+
+// rakamsal değeri tırnak koyarak metinsel değere dönüştürdük.
+
+string deneme = "10";
+
+
+
+
+Console.WriteLine("Adın nedir ?");
+
+
+string name = Console.ReadLine();
+
+
+
+Console.WriteLine("Nerelisin ?");
+
+
+string where = Console.ReadLine();
+
+Console.WriteLine("Kaç yaşındasın");
+
+string age = Console.ReadLine();
+
+Console.WriteLine("Senin adın : {0} Sen yaşadığın şehir  {1} ve sen {2} yaşındasın", name, where, age);
+
+
+
+// değişken isimlendirme kuralları
+
+//string 3222metin = "deger";
+
+string m23dhd = "değer";
+
+
+string tek = "değer";
+
+string coklukelime = "değer";
+
+
+string denemeDegisken = "değer";
+
+
+// türkçe karakter yapmamalısınız
+string üniversite = "değer";
+
+
+// türkçe isimlendirme kullanmaktan kaçınılmalıdır.
+//string ad = "değer";
+
+//string name = "değer";
+
+
+// değiken isimleri değişkenin taşıdığı değer ile ilgili olmalıdır
+
+
+
+// değişken isimlendirmeleri kesinlikle tek karakter olmamalı
+int x = 10;
+
+
+int sdsds = 20;
+
+
+// isimledirme ile ilgili bir örnek 
+int birKelimedenFazlaKelimeTasiyanDegisken = 10;
+
+
+// değişken değersiz tanımlanıp sonrasında değer atanabilir
+string a;
+
+a = "değer";
+
+// aynı tipteki değişkenler yanyana tanımlanabilir daha sonra tek tek değerleri atanır
+string b, c,d,e,f;
+
+b = "değer";
+c = "değer";
+
+
+// çok değişken tanımlama ve değer atama
+int y,z=10;
+
+
+
+// char değişken tanımlama 
+
+char tekHarf =  'A';
+
+
